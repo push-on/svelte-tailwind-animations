ba8f42d959f8f87882d608cf45ca4afe086ca7e9
mhimranhossain <mh.imranhossain@outlook.com>
2023-06-27 10:23:27 +0600

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <div
   class="bg-gradient-to-tr from-slate-950 via-purple-950 to-slate-950 min-h-screen w-full flex justify-center items-center flex-col space-y-10">
-  <h1 class="text-7xl italic font-serif bold">Tailwind Animations</h1>
+  <h1 class="text-3xl sm:text-5xl lg:text-7xl italic font-serif bold">Tailwind Animations</h1>
   <div
-    class="bg-purple-50/5 backdrop-blur-sm p-10 rounded-2xl flex flex-col space-y-5 decoration-purple-800 decoration-4 text-4xl shadow-2xl shadow-black/50">
+    class="bg-purple-50/5 backdrop-blur-sm p-10 rounded-2xl flex flex-col space-y-5 decoration-purple-800 decoration-4 text-xl sm:text-3xl lg:text-4xl shadow-2xl shadow-black/50">
     <a
       class="duration-200 underline underline-offset-4 text-gray-300 hover:text-gray-50 hover:underline-offset-8 hover:decoration-purple-600"
       href="/TextAnim">Text Animation: Slide From Top</a>
     <a
       class="duration-200 underline underline-offset-4 text-gray-300 hover:text-gray-50 hover:underline-offset-8 hover:decoration-purple-600"
       href="/ExpandingDock">Dock Animation: Expand</a>
+    <a
+      class="duration-200 underline underline-offset-4 text-gray-300 hover:text-gray-50 hover:underline-offset-8 hover:decoration-purple-600"
+      href="/ExpandingDock_2">Dock Animation: Expand 2</a>
   </div>
 </div>
---
+++ src/routes/ExpandingDock_2/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  let icons = ["☕", "🍕", "🧁", "🍭", "🍩"]
+</script>
+
+<div
+  class="w-full min-h-screen flex flex-col bg-gradient-to-b from-indigo-800 via-blue-900 to-blue-950 p-10">
+  <div class="">
+    <div
+      class="group fixed bottom-0 left-1/2 -translate-x-1/2 border-t-2 border-white/30 bg-slate-900/40 backdrop-blur-xl m-10 flex justify-center items-center rounded-2xl max-w-fit" >
+        {#each icons as icon}
+          <button
+            class="w-16 h-16 m-2 rounded-lg flex text-4xl cursor-pointer duration-300 justify-center items-center bg-black/20 hover:scale-110 group-active:h-[50vh]"
+            on:click={() => (open = true)}>{icon}
+          </button>
+        {/each}
+    </div>
+  </div>
+</div>
+
