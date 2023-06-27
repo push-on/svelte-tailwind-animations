4f0b88f2cb62c6c38f97723125e8b4e98646cfd4
mhimranhossain <mh.imranhossain@outlook.com>
2023-06-27 12:28:33 +0600

    init

--- src/routes/ExpandingDock_2/+page.svelte
+++ src/routes/ExpandingDock_2/+page.svelte
@@ -1,19 +1,26 @@
-<script>
-  let icons = ["☕", "🍕", "🧁", "🍭", "🍩"]
-</script>
-
 <div
-  class="w-full min-h-screen flex flex-col bg-gradient-to-b from-indigo-800 via-blue-900 to-blue-950 p-10">
-  <div class="">
-    <div
-      class="group fixed bottom-0 left-1/2 -translate-x-1/2 border-t-2 border-white/30 bg-slate-900/40 backdrop-blur-xl m-10 flex justify-center items-center rounded-2xl max-w-fit" >
-        {#each icons as icon}
-          <button
-            class="w-16 h-16 m-2 rounded-lg flex text-4xl cursor-pointer duration-300 justify-center items-center bg-black/20 hover:scale-110 group-active:h-[50vh]"
-            on:click={() => (open = true)}>{icon}
-          </button>
-        {/each}
-    </div>
+  class="w-full min-h-screen flex flex-col bg-gradient-to-b from-sky-800 via-blue-900 to-blue-950 p-10">
+  <div
+    class="group block m-auto relative w-52 p-2 bg-slate-900 text-white text-2xl rounded-xl">
+    <ul
+      class="bg-slate-900 space-y-5 p-2 absolute top-9 left-0 right-0 h-0 duration-500 transition-all ease-in-out w-full group-hover:h-52 group-hover:translate-y-0 group-hover:opacity-100 opacity-0 rounded-b-xl">
+      <li
+        class=" opacity-0 group-hover:opacity-100 transition-opacity delay-500">
+        Child Link
+      </li>
+      <li
+        class=" opacity-0 group-hover:opacity-100 transition-opacity delay-500">
+        Child Link
+      </li>
+      <li
+        class=" opacity-0 group-hover:opacity-100 transition-opacity delay-500">
+        Child Link
+      </li>
+      <li
+        class=" opacity-0 group-hover:opacity-100 transition-opacity delay-500">
+        Child Link
+      </li>
+    </ul>
+    <h1 class="font-serif font-bold">Parent Link</h1>
   </div>
 </div>
-
